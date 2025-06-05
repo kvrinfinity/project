cfe66b29043a43f7fc6012561f537430cb857fb8
Nishan Kamath <nishankamath@gmail.com>
2025-06-05 16:28:12 +0530

    fist

--- database.py
+++ database.py
@@ -1,28 +1,14 @@
 import sqlite3
 
-connection = sqlite3.connect('LoginData.db')
-cursor = connection.cursor()
+conn = sqlite3.connect('LoginData.db')
+cursor = conn.cursor()
 
-cmd1 = """CREATE TABLE IF NOT EXISTS USERS(first_name varchar(50),
-                                          last_name varchar(50),
-                                          email varchar(50) primary key,
-                                          password varchar(50) not null)"""
-
-cursor.execute(cmd1)
-
-cmd3 = """CREATE TABLE IF NOT EXISTS USEROTP(email varchar(50) primary key references USERS(email),otp varchar(6))"""
-cursor.execute(cmd3)
-
-print(cursor.execute("PRAGMA table_info(USEROTP)").fetchall())
-cmd2 = """INSERT INTO USERS(first_name, last_name, email, password) values
-                ('tester','tester','tester@gmail.com','tester')"""
-#cursor.execute(cmd2)
-
-connection.commit()
-
-ans = cursor.execute("select * from USERS").fetchall()
-
-for i in ans:
-    print(i)
-
-connection.close()
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS USERS (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email TEXT UNIQUE,
+    password TEXT
+)
+""")
+conn.commit()
+conn.close()
